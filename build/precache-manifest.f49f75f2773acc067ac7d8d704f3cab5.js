a83ca73d4fa61dff64da4f8649f6c7eb5a17493f
aman <amanmittal0210@gmail.com>
2021-06-21 22:38:05 +0530

    my project

---
+++ build/precache-manifest.f49f75f2773acc067ac7d8d704f3cab5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e4db102001622f55a3a17e353e4e15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b959ee2a6658c0d0299",
+    "url": "/static/js/2.235e61d4.chunk.js"
+  },
+  {
+    "revision": "24985c1faafea80c0085",
+    "url": "/static/js/main.35b2cb51.chunk.js"
+  },
+  {
+    "revision": "583e51a7281672351041",
+    "url": "/static/js/runtime-main.38f647cd.js"
+  }
+]);
